db87d8cc670ba2148fbbae6a0f9e79b760a21e2b
jrobson <jdlrobson@gmail.com>
2014-12-29 15:28:10 -0800

    Fix naming issue to restore ability to switch between editors
    Cherry-picked from master.
    
    Bug: T85480
    Change-Id: I058d0dfa18f2220fc0e2d9ecc8dfd12206d3c481

--- javascripts/modules/editor/VisualEditorOverlay.js
+++ javascripts/modules/editor/VisualEditorOverlay.js
@@ -36,7 +36,7 @@
 			options.previewingMsg = mw.msg( 'mobile-frontend-page-edit-summary', options.title );
 			options.editingMsg = mw.msg( 'mobile-frontend-editor-editing' );
 			EditorOverlayBase.prototype.initialize.apply( this, arguments );
-			this.hasChanged = false;
+			this._hasChanged = false;
 			this.$continueBtn = self.$( '.continue' ).prop( 'disabled', true );
 			this.initializeSwitcher();
 		},
@@ -105,7 +105,7 @@
 			this.$( '.back' ).on( 'click', $.proxy( this, 'switchToEditor' ) );
 			this.$( '.source-editor' ).on( 'click', function () {
 				// If changes have been made tell the user they have to save first
-				if ( !self.hasChanged ) {
+				if ( !self.hasChanged() ) {
 					self.switchToSourceEditor( options );
 				} else {
 					if ( window.confirm( mw.msg( 'mobile-frontend-editor-switch-confirm' ) ) ) {
@@ -151,7 +151,7 @@
 			}
 			this._showHidden( '.saving-header' );
 			// Stop the confirmation message from being thrown when you hit save.
-			this.hasChanged = false;
+			this._hasChanged = false;
 			this.$( '.surface, .summary-area' ).hide();
 			if ( this.captchaId ) {
 				// Intentional Lcase ve save api properties
@@ -213,7 +213,7 @@
 		 * Event handler.
 		 */
 		onTransact: function () {
-			this.hasChanged = true;
+			this._hasChanged = true;
 			this.$continueBtn.prop( 'disabled', false );
 		},
 		/**
@@ -254,8 +254,8 @@
 			this._showCaptcha( editApi.captcha.url );
 		},
 		/** @inheritdoc **/
-		_hasChanged: function () {
-			return this.hasChanged;
+		hasChanged: function () {
+			return this._hasChanged;
 		}
 	} );
 
